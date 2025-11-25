1dcbd0583c5e4e18fe98ab51c26b9e745096e42e
Alks24 <al.ambatiello@gmail.com>
2025-11-25 16:38:48 +0100

    before pull

--- static/scripts/create.js
+++ static/scripts/create.js
@@ -1,2 +1,21 @@
 //work in progress
 
+$('#party_submit').click(function(){
+    const address = $('#party_address').val();
+    const title = $('#party_title').val();
+    const description = $('#party_description"').val();
+
+    if(address === "" || title === "" || description === ""){
+        alert("Veuillez remplir tous les champs pour soumettre");
+        return;
+    }
+
+ $.post("/create", {address: address, title: title, description: description}, function(data) {
+    if(data.success) {
+        alert(data.message);
+        window.location.href = "/";
+    } else {
+        alert(data.message);
+    } 
+ })
+})
